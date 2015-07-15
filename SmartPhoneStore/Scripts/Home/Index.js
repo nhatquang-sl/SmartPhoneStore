1fda7c070510c94392b94b64f49df6b74c99bc7a
nhatquang-sl <sunlight479@yahoo.com>
2015-07-16 00:43:09 +0700

    refactor code

--- SmartPhoneStore/Scripts/Home/Index.js
+++ SmartPhoneStore/Scripts/Home/Index.js
@@ -25,7 +25,28 @@
     var SwipeCards = function (element, listImage) {
         this.element = element;
         this.listImage = listImage;
+        this.animate = {
+            zoomIn: "zoomInCard", zoomOut: "zoomOutCard"
+                , fadeOutLeft: "fadeOutLeftCard", fadeOutRight: "fadeOutRightCard"
+                , fadeOutUp: "fadeOutUpCard", fadeOutDown: "fadeOutDownCard", fadeInBack: "fadeInBackCard"
+        };
 
+        this.fadeOutLeftCard = [];
+        this.fadeOutRightCard = [];
+        this.fadeOutUpCard = [];
+        this.fadeOutDownCard = [];
+        this.currentCard;
+        this.nextCard;
+        this.index = 0;
+        this.left = 0;
+        this.right = 0;
+        this.up = 0;
+        this.down = 0;
+        this.speed;
+        this.remainDistance;
+        this.remainDuration;
+        this.proportion;
+        this.startPhase;
     }
 
     SwipeCards.prototype = {
@@ -47,87 +68,89 @@
                 "width": "100%"
             });
 
+            var ss = document.styleSheets;
+            for (var i = 0; i < ss.length; ++i) {
+                for (var x = 0; x < ss[i].cssRules.length; ++x) {
+                    if (ss[i].cssRules[x].type == CSSRule.KEYFRAMES_RULE) {
+                        switch (ss[i].cssRules[x].name) {
+                            case this.animate.fadeOutLeft:
+                                this.fadeOutLeftCard.push(ss[i].cssRules[x]);
+                                break;
+                            case this.animate.fadeOutRight:
+                                this.fadeOutRightCard.push(ss[i].cssRules[x]);
+                                break;
+                            case this.animate.fadeOutUp:
+                                this.fadeOutUpCard.push(ss[i].cssRules[x]);
+                                break;
+                            case this.animate.fadeOutDown:
+                                this.fadeOutDownCard.push(ss[i].cssRules[x]);
+                                break;
+                        }
+                    }
+                }
+            }
+
             this.swipeStart();
         },
         swipeStart: function () {
             var listCard = this.element.children();
-            var index = 0;
-            var animate = {
-                zoomIn: "zoomInCard", zoomOut: "zoomOutCard"
-                , fadeOutLeft: "fadeOutLeftCard", fadeOutRight: "fadeOutRightCard"
-                , fadeOutUp: "fadeOutUpCard", fadeOutDown: "fadeOutDownCard", fadeInBack: "fadeInBackCard"
-            };
-            var startPhase = null;
-            var left = 0;
-            var right = 0;
-            var up = 0;
-            var down = 0;
+
+            var self = this;
 
             this.element.swipe({
                 swipeStatus: function (event, phase, direction, distance, duration, fingers) {
                     console.log(phase + " " + direction + " " + distance + " " + duration);
-                    var curr = listCard[index];
-                    var next = listCard[index + 1];
-                    var proportion = distance / $(curr).width();
+                    self.currentCard = listCard[self.index];
+                    self.nextCard = listCard[self.index + 1];
+                    self.proportion = distance / $(self.currentCard).width();
 
                     switch (phase) {
                         case "start":
-                            this.startPhase = direction;
-                            $(curr).removeClass(animate.zoomIn);
-                            $(curr).removeClass(animate.fadeInBack);
-                            $(curr).css({
-                                "-webkit-transform": "scale3d(1, 1, 1)",
-                                "transform": "scale3d(1, 1, 1)",
-                                "opacity": "1"
-                            });
-
-                            $(next).removeClass(animate.zoomOut);
-                            $(next).css({
-                                "-webkit-transform": "scale3d(.1, .1, .1)",
-                                "transform": "scale3d(.1, .1, .1)",
-                                "opacity": "0"
-                            });
-
+                            self.startSwipe(direction);
                             break;
                         case "move":
-                            if (startPhase == null) startPhase = direction;
-                            switch (startPhase) {
+                            if (self.startPhase == null) self.startPhase = direction;
+                            switch (self.startPhase) {
                                 case "left":
-                                    $(curr).css({
+                                    self.remainDistance = $(self.currentCard).width() - distance;
+                                    $(self.currentCard).css({
                                         "-webkit-transform": "translate3d(-" + distance + "px, 0%, 0)",
                                         "transform": "translate3d(-" + distance + "px, 0%, 0)",
-                                        "opacity": 1 - proportion
+                                        "opacity": 1 - self.proportion
                                     });
                                     break;
                                 case "right":
-                                    $(curr).css({
+                                    self.remainDistance = $(self.currentCard).width() - distance;
+                                    $(self.currentCard).css({
                                         "-webkit-transform": "translate3d(" + distance + "px, 0%, 0)",
                                         "transform": "translate3d(" + distance + "px, 0%, 0)",
-                                        "opacity": 1 - proportion
+                                        "opacity": 1 - self.proportion
                                     });
                                     break;
                                 case "up":
-                                    proportion = distance / $(curr).height();
-                                    $(curr).css({
+                                    self.remainDistance = $(self.currentCard).height() - distance;
+                                    self.proportion = distance / $(self.currentCard).height();
+                                    $(self.currentCard).css({
                                         "-webkit-transform": "translate3d(0, -" + distance + "px, 0)",
                                         "transform": "translate3d(0, -" + distance + "px, 0)",
-                                        "opacity": 1 - proportion
+                                        "opacity": 1 - self.proportion
                                     });
                                     break;
                                 case "down":
-                                    proportion = distance / $(curr).height();
-                                    $(curr).css({
+                                    self.remainDistance = $(self.currentCard).height() - distance;
+                                    self.proportion = distance / $(self.currentCard).height();
+                                    $(self.currentCard).css({
                                         "-webkit-transform": "translate3d(0, " + distance + "px, 0)",
                                         "transform": "translate3d(0, " + distance + "px, 0)",
-                                        "opacity": 1 - proportion
+                                        "opacity": 1 - self.proportion
                                     });
                                     break;
                             }
-                            if (startPhase != null) {
-                                $(next).css({
-                                    "-webkit-transform": "scale3d(" + proportion + ", " + proportion + ", " + proportion + ")",
-                                    "transform": "scale3d(" + proportion + ", " + proportion + ", " + proportion + ")",
-                                    "opacity": proportion
+                            if (self.startPhase != null) {
+                                $(self.nextCard).css({
+                                    "-webkit-transform": "scale3d(" + self.proportion + ", " + self.proportion + ", " + self.proportion + ")",
+                                    "transform": "scale3d(" + self.proportion + ", " + self.proportion + ", " + self.proportion + ")",
+                                    "opacity": self.proportion
                                 });
                             }
                             try {
@@ -135,6 +158,9 @@
                             } catch (error) {
                                 endSwipePosition = event;
                             }
+                            self.speed = duration / distance;
+                            self.remainDuration = self.speed * self.remainDistance;
+
                             break;
                         case "end":
                             var startSwipeScreenPosition = $(this).position();
@@ -144,40 +170,104 @@
                                 || endSwipePosition.pageY < startSwipeScreenPosition.top
                                 || endSwipePosition.pageY > bottomSwipeScreenPosition)
                                 || (distance > $(this).swipe("option", "threshold") && duration < 200)) {
-                                switch (startPhase) {
+                                console.log(self.startPhase);
+                                self.remainDuration /= 1000;
+                                $(".card").css({
+                                    "-webkit-animation-duration": self.remainDuration + "s",
+                                    "animation-duration": self.remainDuration + "s"
+                                });
+                                switch (self.startPhase) {
                                     case "left":
-                                        $(curr).addClass(animate.fadeOutLeft);
-                                        $(next).addClass(animate.zoomIn);
-                                        left++;
+                                        self.moveOutLeft();
                                         break;
                                     case "right":
-                                        $(curr).addClass(animate.fadeOutRight);
-                                        $(next).addClass(animate.zoomIn);
-                                        right++;
+                                        self.moveOutRight();
                                         break;
                                     case "up":
-                                        $(curr).addClass(animate.fadeOutUp);
-                                        $(next).addClass(animate.zoomIn);
-                                        up++;
+                                        self.moveOutUp();
                                         break;
                                     case "down":
-                                        $(curr).addClass(animate.fadeOutDown);
-                                        $(next).addClass(animate.zoomIn);
-                                        down++;
+                                        self.moveOutDown();
                                         break;
                                 }
-                                if (startPhase != null) {
-                                    index++;
+                                if (self.startPhase != null) {
+                                    $(self.nextCard).addClass(self.animate.zoomIn);
+                                    self.index++;
                                 }
                             } else {
-                                $(curr).addClass(animate.fadeInBack);
-                                $(next).addClass(animate.zoomOut);
+                                $(".card").css({
+                                    "-webkit-animation-duration": "1s",
+                                    "animation-duration": "1s"
+                                });
+                                $(self.currentCard).addClass(self.animate.fadeInBack);
+                                $(self.nextCard).addClass(self.animate.zoomOut);
                             }
-                            startPhase = null;
+                            self.startPhase = null;
                             break;
                     }
                 }, threshold: 10
             });//end of swipe
+        },
+        startSwipe: function (direction) {
+            this.startPhase = direction;
+            $(this.currentCard).removeClass(this.animate.zoomIn);
+            $(this.currentCard).removeClass(this.animate.fadeInBack);
+            $(this.currentCard).css({
+                "-webkit-transform": "scale3d(1, 1, 1)",
+                "transform": "scale3d(1, 1, 1)",
+                "opacity": "1"
+            });
+
+            $(this.nextCard).removeClass(this.animate.zoomOut);
+            $(this.nextCard).css({
+                "-webkit-transform": "scale3d(.1, .1, .1)",
+                "transform": "scale3d(.1, .1, .1)",
+                "opacity": "0"
+            });
+        },
+        moveOutLeft: function () {
+            for (var i = 0; i < this.fadeOutLeftCard.length; i++) {
+                this.fadeOutLeftCard[i].deleteRule("100%");
+
+                this.fadeOutLeftCard[i].appendRule("100% { opacity: 0; -webkit-transform: translate3d(-"
+                     + this.remainDistance + "px, 0, 0); transform: translate3d(-"
+                     + this.remainDistance + "px, 0, 0);}");
+                $(this.currentCard).addClass(this.animate.fadeOutLeft);
+                this.left++;
+            }
+        }, moveOutRight: function () {
+            for (var i = 0; i < this.fadeOutRightCard.length; i++) {
+                this.fadeOutRightCard[i].deleteRule("100%");
+
+                this.fadeOutRightCard[i].appendRule("100% { opacity: 0; -webkit-transform: translate3d("
+                     + this.remainDistance + "px, 0, 0); transform: translate3d("
+                     + this.remainDistance + "px, 0, 0);}");
+
+                $(this.currentCard).addClass(this.animate.fadeOutRight);
+                this.right++;
+            }
+        }, moveOutUp: function () {
+            for (var i = 0; i < this.fadeOutUpCard.length; i++) {
+                this.fadeOutUpCard[i].deleteRule("100%");
+
+                this.fadeOutUpCard[i].appendRule("100% { opacity: 0; -webkit-transform: translate3d(0, -"
+                     + this.remainDistance + "px, 0); transform: translate3d(0, -"
+                     + this.remainDistance + "px, 0);}");
+
+                $(this.currentCard).addClass(this.animate.fadeOutUp);
+                this.up++;
+            }
+        }, moveOutDown: function () {
+            for (var i = 0; i < this.fadeOutDownCard.length; i++) {
+                this.fadeOutDownCard[i].deleteRule("100%");
+
+                this.fadeOutDownCard[i].appendRule("100% { opacity: 0; -webkit-transform: translate3d(0, "
+                     + this.remainDistance + "px, 0); transform: translate3d(0, "
+                     + this.remainDistance + "px, 0);}");
+
+                $(this.currentCard).addClass(this.animate.fadeOutDown);
+                this.down++;
+            }
         }
 
     };
